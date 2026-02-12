29279e6f0597d684b36918c3dfcade49a0d04537
awliren@gmail.com <awliren@gmail.com>
2026-02-12 16:21:48 +0800

    removed debug files

--- Debug/source/semihost_hardfault.d
+++
@@ -1,4 +0,0 @@
-source/semihost_hardfault.o source/semihost_hardfault.d: \
- ../source/semihost_hardfault.c \
- C:\MCUexpresso\aeviceMD12\rdrw612bga_flexcomm_spi_polling_b2b_transfer_master\source\mcux_config.h
-C:\MCUexpresso\aeviceMD12\rdrw612bga_flexcomm_spi_polling_b2b_transfer_master\source\mcux_config.h:
--- Debug/startup/startup_rw612.d
+++
@@ -1,4 +0,0 @@
-startup/startup_rw612.o startup/startup_rw612.d: \
- ../startup/startup_rw612.c \
- C:\MCUexpresso\aeviceMD12\rdrw612bga_flexcomm_spi_polling_b2b_transfer_master\source\mcux_config.h
-C:\MCUexpresso\aeviceMD12\rdrw612bga_flexcomm_spi_polling_b2b_transfer_master\source\mcux_config.h:
